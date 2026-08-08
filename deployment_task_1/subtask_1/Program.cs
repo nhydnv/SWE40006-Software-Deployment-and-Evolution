d92df81ab9e1d4415269906968a8bbc10b3fee3b
mungkeumeomeo <nhido.1911@gmail.com>
2026-08-09 00:32:22 +1000

    task 1.1

---
+++ deployment_task_1/subtask_1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace subtask_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Deployment Activity 1: Pass Task Completed!");
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+
+    }
+}
